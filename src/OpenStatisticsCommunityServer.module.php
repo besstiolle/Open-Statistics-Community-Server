ea5e16ba14f358aaf111a0edb16fb68b6c71996d
besstiolle <besstiolle@gmail.com>
2011-01-09 15:11:28 +0100

    update licence

--- src/OpenStatisticsCommunityServer.module.php
+++ src/OpenStatisticsCommunityServer.module.php
@@ -1,4 +1,37 @@
 <?php
+#-------------------------------------------------------------------------
+# Module: OSCS - XXX
+#
+# Version: de Kevin Danezis Aka "Bess"
+# Author can be join on the french forum : http://www.cmsmadesimple.fr/forum 
+#        or by email : contact [plop] furie [plap] be
+#
+# The module's download page is : N/A
+# The module's demo page is : http://www.cmsmadesimple.fr/statistiques
+#
+# The discussion page around the module : http://www.cmsmadesimple.fr/forum/viewtopic.php?id=2908
+# The author's GIT page is : https://github.com/besstiolle
+# The module's GIT page is : https://github.com/besstiolle/Open-Statistics-Community-Server
+# The module's SVN page is : N/A
+#
+#-------------------------------------------------------------------------
+# CMS - CMS Made Simple is (c) 2004-2011 by Ted Kulp (wishy@cmsmadesimple.org)
+# This project's homepage is: http://www.cmsmadesimple.org
+#-------------------------------------------------------------------------
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+# Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
+#-------------------------------------------------------------------------
 
 $cgextensions = cms_join_path($gCms->config['root_path'],'modules',
 			      'CGExtensions','CGExtensions.module.php');
